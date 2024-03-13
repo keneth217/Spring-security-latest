c2ed81137100cd2393406562cdc5dd71671fe7b4
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-13 17:36:09 +0300

    controller

--- src/main/java/com/example/security/repositories/UserRepository.java
+++ src/main/java/com/example/security/repositories/UserRepository.java
@@ -1,9 +1,15 @@
 package com.example.security.repositories;
 
+import com.example.security.entities.Role;
 import com.example.security.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
+    Optional<User> findByEmail(String username);
+    User findByRole(Role role);
 }
