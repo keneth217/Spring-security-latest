d62c5b27571a10942d3d5a9eebaa57a922197157
keneth217 <kipyegonkeneth03@gmail.com>
2024-03-13 17:36:03 +0300

    controller

---
+++ src/main/java/com/example/security/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.example.security.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/admin")
+public class AdminController {
+    @GetMapping
+    public ResponseEntity<String> admin(){
+        return  ResponseEntity.ok("welcome admin");
+    }
+}
