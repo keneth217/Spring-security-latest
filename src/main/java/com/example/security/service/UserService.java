7f51b3adf1a27ba9c33f961f982995c011e7bfa1
keneth217 <kipyegonkeneth03@gmail.com>
2024-05-07 07:40:45 +0300

    jwt

--- src/main/java/com/example/security/service/UserService.java
+++
@@ -1,7 +0,0 @@
-package com.example.security.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-public interface UserService {
-    public UserDetailsService userDetailsService();
-}
