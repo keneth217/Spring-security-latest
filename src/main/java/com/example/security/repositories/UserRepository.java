2bc1e2ee90c7e66b7f178a41bb4f6eba94998ee4
keneth217 <kipyegonkeneth03@gmail.com>
2024-05-07 07:40:35 +0300

    jwt

--- src/main/java/com/example/security/repositories/UserRepository.java
+++ src/main/java/com/example/security/repositories/UserRepository.java
@@ -1,15 +1,18 @@
 package com.example.security.repositories;
 
-import com.example.security.entities.Role;
+
 import com.example.security.entities.User;
+import com.example.security.enums.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
+
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User,Long> {
-    Optional<User> findByEmail(String username);
+    Optional<User> findByEmail(String email);
     User findByRole(Role role);
+
+//    User findFirstByEmail(String email);
 }
